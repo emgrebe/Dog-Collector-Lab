bb996df1e5d6f1651dda6897cf44b06d52adb63f
Emily Landgrebe <emilandgrebe@gmail.com>
2019-09-09 14:25:18 -0500

    functional and minimal

--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,19 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def dogs_index(request):
+  return render(request, 'dogs/index.html', {'dogs': dogs})
+
+class Dog:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed 
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Ace', 'Pit/Lab', 'Tan', 4),
+  Dog('Chloe', 'Boxer', 'Nub tail', 13),
+  Dog('Diesel', 'Brindel', 'Dosile', 8)
+]
