4d741c53e856fc34f5d3bf9e072aaa06e46b83ed
Emily Landgrebe <emilandgrebe@gmail.com>
2019-09-09 16:18:47 -0500

    add admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from.models import Dog
 
 
 def home(request):
   return render(request, 'home.html')
-
 def about(request):
   return render(request, 'about.html')
-
 def dogs_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', {'dogs': dogs})
-
-class Dog:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed 
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Ace', 'Pit/Lab', 'Tan', 4),
-  Dog('Chloe', 'Boxer', 'Nub tail', 13),
-  Dog('Diesel', 'Brindel', 'Dosile', 8)
-]
