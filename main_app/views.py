b3ac85530308f5265698d4ed1a2d1d3f66d3ab5d
Emily Landgrebe <emilandgrebe@gmail.com>
2019-09-09 15:14:10 -0500

    add dog model

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>Hello<h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
