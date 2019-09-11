2915bbf00b3b1e708d018703740d7a4ed7af8e00
Emily Landgrebe <emilandgrebe@gmail.com>
2019-09-11 16:46:13 -0500

    add feeding message

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
+from .forms import FeedingForm
 
 class DogCreate(CreateView):
   model = Dog
@@ -17,11 +18,26 @@ class DogDelete(DeleteView):
 
 def home(request):
   return render(request, 'home.html')
+
 def about(request):
   return render(request, 'about.html')
+
 def dogs_index(request):
   dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', {'dogs': dogs})
+
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
-  return render(request, 'dogs/detail.html', {'dog': dog})
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', {
+    'dog': dog, 'feeding_form': feeding_form,
+  })
+
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
+
